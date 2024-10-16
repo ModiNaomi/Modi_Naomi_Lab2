50856b8dce9c69d67380f94e7cf8897fd693628f
Naomi Modi <Naomi Modi@DESKTOP-K8KUAG6>
2024-10-16 20:29:59 +0300

    Add project files.

---
+++ Data/Modi_Naomi_Lab2Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Modi_Naomi_Lab2.Models;
+
+namespace Modi_Naomi_Lab2.Data
+{
+    public class Modi_Naomi_Lab2Context : DbContext
+    {
+        public Modi_Naomi_Lab2Context (DbContextOptions<Modi_Naomi_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Modi_Naomi_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Modi_Naomi_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+    }
+}
